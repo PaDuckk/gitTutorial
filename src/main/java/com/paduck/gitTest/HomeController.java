08e97cc6ef924c7a6c0efdedf62534a89d91b462
PaDuckk <kang10019@naver.com>
2018-10-18 13:57:52 +0900

    add sysout hi

--- src/main/java/com/paduck/gitTest/HomeController.java
+++ src/main/java/com/paduck/gitTest/HomeController.java
@@ -34,7 +34,7 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 
 		
-
+		System.out.println("hi");
 		System.out.println("!?3");
 
 		System.out.println("metoo");
